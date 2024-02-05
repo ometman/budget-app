f24a67b93dc805bf98cdeb1082c24c935255901a
ometman <revomet@gmail.com>
2024-02-05 17:37:15 +0000

    Generate controllers

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "should get Deals" do
+    get categories_Deals_url
+    assert_response :success
+  end
+end
